5990d65dcd38f6b3dd11fa8619d95b38a57e70bb
sunday <ogbonnasundaycy@gmail.com>
2025-09-02 07:53:55 +0100

    debug

--- frontend/src/pages/api.js
+++ frontend/src/pages/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_API_URL;
+console.log(`API URL: ${BASE_URL}`);
 
-console.log(BASE_URL);
 
 const api = axios.create({
     origin: BASE_URL,
