6cb9a5db0dbf31461e581de1fc9d9d06cff7d7a4
sunny <ogbonnasundaycy@gmail.com>
2025-09-30 10:18:40 +0100

    update

--- frontend/src/pages/Navigation.jsx
+++ frontend/src/pages/Navigation.jsx
@@ -183,8 +183,6 @@ const Navigation = () => {
 
     const handleAuth = () => {
       const user = JSON.parse(localStorage.getItem('user'));
-      navigate('/admin');
-      return
       if (user) {
           window.open('/logout', '_blank');
       }
