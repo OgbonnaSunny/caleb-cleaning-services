c1c1cc3c03fa4a71c60c151dcadd13026cff54d5
sunny <ogbonnasundaycy@gmail.com>
2025-10-04 12:19:52 +0100

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -48,13 +48,6 @@ export default defineConfig(({ mode }) => {
           assetFileNames: 'assets/[name]-[hash][extname]',
           manualChunks(id) {
             if (id.includes('node_modules')) {
-              const lib = id.toString().split('node_modules/')[1].split('/')[0];
-              if (['react', 'react-dom', 'react-router'].includes(lib)) {
-                return 'vendor-react';
-              }
-              if (lib.includes('chart') || lib.includes('vis')) {
-                return 'vendor-charts';
-              }
               return 'vendor';
             }
           }
