85308bb0ccd00681aed4a7fce7d9a75f0c2b36ff
sunny <ogbonnasundaycy@gmail.com>
2025-10-06 21:16:11 +0100

    update

--- frontend/src/pages/Navigation.jsx
+++ frontend/src/pages/Navigation.jsx
@@ -183,6 +183,8 @@ const Navigation = () => {
 
     const handleAuth = () => {
       const user = JSON.parse(localStorage.getItem('user'));
+      navigate('/cleanerprofile');
+      return;
       if (user) {
           window.open('/logout', '_blank');
       }
